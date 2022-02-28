686540003859920b909495a89780a6290d74bb5d
jasonDuchateau <95851639+jasonDuchateau@users.noreply.github.com>
2022-02-28 16:30:10 +0100

    Fixing Issue TODO : fermer la modale #1

--- starterOnly/script/modal.js
+++ starterOnly/script/modal.js
@@ -11,15 +11,53 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block"
+
+
+  firstname.addEventListener("keyup", function (event) {
+
+    if (firstname.value.length >= 2) {
+      firstname.setCustomValidity("");
+    } else {
+      firstname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    }
+  });
+
+
+  lastname.addEventListener("keyup", function (event) {
+
+    if (lastname.value.length >= 2) {
+      lastname.setCustomValidity("");
+    } else {
+      lastname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    }
+  });
+
+  BrthDate.oninvalid = function (e) {
+    e.target.setCustomValidity("");
+    if (!e.target.validity.valid) {
+      e.target.setCustomValidity("Vous devez entrer votre date de naissance.");
+    }
+  };
+  BrthDate.oninput = function (e) {
+    e.target.setCustomValidity("");
+  };
+
+
+
+
+
 }
 function validate() {
 
 
+  var formData = new FormData(formData)
 
 }
+
