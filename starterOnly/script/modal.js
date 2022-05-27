3ade931b82dba70548c78eaf92bbfc8dc444456f
jasonDuchateau <95851639+jasonDuchateau@users.noreply.github.com>
2022-05-27 07:35:50 +0200

    error display 'other way)

--- starterOnly/script/modal.js
+++ starterOnly/script/modal.js
@@ -21,12 +21,18 @@ var firstname = document.getElementById("first");
 var lastname = document.getElementById("last");
 var BrthDate = document.getElementById("birthdate");
 var email = document.getElementById("email");
+var checkbox1 = document.getElementById("checkbox1");
 
+var LocationTournament = document.querySelector('#location1');
 var canOpenEditNav = true;
 
-lastname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
 
-BrthDate.setCustomValidity("Vous devez entrer votre date de naissance.");
+ firstname.setCustomValidity("");
+ lastname.setCustomValidity("");
+ BrthDate.setCustomValidity("");
+ email.setCustomValidity("");
+ checkbox1.setCustomValidity("");
+ LocationTournament.setCustomValidity("");
 
 document.getElementById('thank-you').style.display = "none";
 
@@ -40,30 +46,88 @@ function launchModal() {
   canOpenEditNav = false;
   modalbg.style.display = "block"
 
+  let firstnameInputError = document.querySelector('#FirstNameError');
+  let lastnameInputError = document.querySelector('#LastNameError');
+  let BirthInputError = document.querySelector('#BirthError');
+  let EmailInputError = document.querySelector('#MailError');
+  let TournamentInputError = document.querySelector('#TournamentError');
+  let checkboxInputError = document.querySelector('#checkboxError');
+  
+  
 
   firstname.addEventListener("keyup", function (event) {
 
     if (firstname.value.length >= 2) {
-      firstname.setCustomValidity("");
+      //firstname.setCustomValidity("");
+      firstnameInputError.style.display = "none";
     } else {
-      firstname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+      //firstname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+      firstnameInputError.style.display = "block";
     }
   });
 
+  firstname.oninvalid = function (e) {
+    if (firstname.value.length >= 2) {
+      //firstname.setCustomValidity("");
+      firstnameInputError.style.display = "none";
+    } else {
+      //firstname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+      firstnameInputError.style.display = "block";
+    }
+  };
+
+  lastname.oninvalid = function (e) {
+    if (firstname.value.length >= 2) {
+      //firstname.setCustomValidity("");
+      lastnameInputError.style.display = "none";
+    } else {
+      //firstname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+      lastnameInputError.style.display = "block";
+    }
+  };
 
   lastname.addEventListener("keyup", function (event) {
-
+ 
     if (lastname.value.length >= 2) {
-      lastname.setCustomValidity("");
+      lastnameInputError.style.display = "none";
+      //lastname.setCustomValidity("");
     } else {
-      lastname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+      lastnameInputError.style.display = "block";
+      //lastname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    }
+  });
+
+  LocationTournament.oninvalid = function (e) {
+    TournamentInputError.style.display = "none";
+    if (!e.target.validity.valid) {
+      TournamentInputError.style.display = "block";
+    }
+  };
+
+  checkbox1.oninvalid = function (e) {
+    checkboxInputError.style.display = "none";
+    if (!e.target.validity.valid) {
+      checkboxInputError.style.display = "block";
+    }
+  };
+
+  email.addEventListener("keyup", function (event) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+      EmailInputError.style.display = "none";
+      //lastname.setCustomValidity("");
+    } else {
+      EmailInputError.style.display = "block";
+      //lastname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
     }
   });
 
   BrthDate.oninvalid = function (e) {
-    e.target.setCustomValidity("");
-    if (!e.target.validity.valid) {
-      e.target.setCustomValidity("Vous devez entrer votre date de naissance.");
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+      EmailInputError.style.display = "none";
+      //lastname.setCustomValidity("");
+    } else {
+      EmailInputError.style.display = "block";
+      //lastname.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
     }
   };
   BrthDate.oninput = function (e) {
@@ -109,6 +173,6 @@ function validate(event) {
 function closeThank() {
   canOpenEditNav = true;
   document.getElementById('thank-you').style.display = "none";
-  document.querySelector('.hero-section').style.display = "initial";
+  document.querySelector('.hero-section').style.display = "block";
 
 }
